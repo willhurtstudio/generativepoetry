d0b94f351d1cce3d274cfd259a40990e621196db
willhurtstudio <115721924+willhurtstudio@users.noreply.github.com>
2022-10-18 11:40:09 +0100

    Multiple Poem Generators

--- myGenerativePoetry.js
+++ myGenerativePoetry.js
@@ -14,12 +14,21 @@ console.log(opening + randomStringFromArray(article) + randomStringFromArray(ver
 
 
 // this is a for loop, it will run the code in curly brackets 10 times
-for(let i = 0; i < 10; i++){
-    console.log(opening + randomStringFromArray(article) + randomStringFromArray(verbs) + randomStringFromArray(adjectives))
-}
+//for(let i = 0; i < 10; i++){
+//    console.log(opening + randomStringFromArray(article) + randomStringFromArray(verbs) + randomStringFromArray(adjectives))
+//}
+
+
+// this creates a variable that contains the poem
+//let output = opening + randomStringFromArray(article) + randomStringFromArray(verbs) + randomStringFromArray(adjectives)
+// this pushes the poem back out to the HTML of the site
+//document.getElementById("para").innerHTML = output;
+
+
 
 // this is a function to return a random string from an array of strings
 function randomStringFromArray(inputArray){
     let randomNumber = Math.floor(Math.random()*inputArray.length)
     return inputArray[randomNumber]
 }
+
